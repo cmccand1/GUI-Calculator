458604ebfae73f587d5ff7d9d87908c401cf6615
Clint McCandless <clint.mccandless@gmail.com>
2018-01-04 07:22:02 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+package calculator;
+
+
+/**
+ *
+ * @author clintmccandless
+ */
+public class Main {
+    
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        GUI calc = new GUI();
+        calc.display();
+        
+        
+    } // end of main
+
+} // end of class
