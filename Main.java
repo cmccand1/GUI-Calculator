714d537bf11037241481a684ca33f5a46ffead4b
Clint McCandless <clint.mccandless@gmail.com>
2018-01-11 20:51:26 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui.calculator;
 
 import java.awt.BorderLayout;
@@ -21,9 +16,8 @@ public class Main {
     public static void main(String[] args)
     {
         JFrame frame = new JFrame("Calculator");
-        frame.getContentPane().setBackground(Color.BLACK);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
+        frame.setResizable(true);
         frame.setLayout(new BorderLayout());
         frame.add(new GUICalculator(), BorderLayout.CENTER);
         frame.pack();
